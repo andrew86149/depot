1693849b47936ff6653f27a1f5617fe40cde588c
Andrew <a.abramov@mkpteplo.ru>
2018-06-05 10:18:07 +0300

    Add mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,17 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  #config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :test
+  
+  config.action_mailer.smtp_settings = {
+      address:          "smtp.yandex.ru",
+      port:             465,
+      domain:           "mkpteplo.ru",
+      authentication:   "plain",
+      user_name:        "a.abramov@mkpteplo.ru",
+      password:         "An77103",
+      enable_starttls_auto: true
+  }
+  
 end
