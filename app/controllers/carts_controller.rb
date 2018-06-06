45eb90a28915934d33a55882525ecfbe71b250b8
Andrew <a.abramov@mkpteplo.ru>
2018-06-06 10:12:45 +0300

    Add User Sessions Admin

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,5 @@
 class CartsController < ApplicationController
+  #skip_before_action :authorize, only: [:create, :update, :destroy]
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
   rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
 
@@ -75,8 +76,8 @@ class CartsController < ApplicationController
       params[:cart]
     end
     
-    def invalid_cart
-      logger.error "Attempt to access invalid cart #{params[:id]}"
-      redirect_to store_index_url, notice: 'Invalid cart'
-    end
+#    def invalid_cart
+#      logger.error "Attempt to access invalid cart #{params[:id]}"
+#      redirect_to store_index_url, notice: 'Invalid cart'
+#    end
 end
