7e4e2a33a38947355bf182effd2b27781d041c67
Andrew <a.abramov@mkpteplo.ru>
2018-06-06 12:31:05 +0300

    Final authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  #before_action :authorize
+  before_action :authorize
   protect_from_forgery with: :exception
 
-#  protected
+  protected
   
-#    def authorize
-#      unless User.find_by(id: session[:user_id])
-#      redirect_to login_url, notice: "Пожалуйста, пройдите авторизацию"
-#      end
-#    end
+    def authorize
+      unless User.find_by(id: session[:user_id])
+        redirect_to login_url, notice: "Пожалуйста, пройдите авторизацию"
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   get 'admin' => 'admin#index'
-  controller :session do
+  controller :sessions do
     get 'login' => :new
     post 'login' => :create
     delete 'logout' => :destroy
   end
   #get 'admin/index'
 
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  #get 'sessions/new'
+  #get 'sessions/create'
+  #get 'sessions/destroy'
 
   resources :users
   resources :orders
